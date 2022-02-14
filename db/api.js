21b15a2b88ed03ec40c439399f9bbe59d20e9753
Ragil14 <rahil.gilani@gmail.com>
2022-02-14 12:33:49 -0600

    create api.js file & add class constructor to it

---
+++ db/api.js
@@ -0,0 +1,16 @@
+const connection = require('./connection');
+
+class DB { 
+    constructor(connection){
+        this.connection = connection;
+    }
+    
+    viewAllEmployees(){
+        return this.connection.query (`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;`);
+    };
+    createEmployee(employee){
+        return this.connection.query(`INSERT INTO employee SET ?`, employee);
+    };
+}
+
+module.exports = new DB(connection);
